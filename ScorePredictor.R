f9c8adeda7da47fc539ebe7b6ebe425952d2663a
satkriti <30426128+satkriti@users.noreply.github.com>
2022-06-08 11:45:10 +0530

    Create ScorePredictor.R

---
+++ ScorePredictor.R
@@ -0,0 +1,58 @@
+#Q1: Problem statement: PREDICT PERCENTAGE BASED ON STUDY HOURS
+#Q2: WHAT IS PREDICTED SCORE IF STUDENT STUDIES 9.25 HOURS PER DAY
+
+s_data = read.csv("studentscore.csv")
+#EDA: 
+
+dim(s_data)
+head(s_data)
+str(s_data)
+#no missing vals
+apply(s_data, 2, function(x) sum(is.na(x)))
+
+#prop.table(table(s_data$Scores))  ///////no imbalance
+#prop.table(table(s_data$Hours))   ///////no imbalance
+
+summary(s_data)
+
+#understanding:score is the DV/RV and hours is independent
+library(ggplot2)
+ggplot(data = s_data, aes(x = Scores, y = Hours)) + geom_boxplot() #no outliers
+
+#can we build a Linear Reg model?
+ggplot(data = s_data, aes(x = Scores, y = Hours)) + geom_point() #yes we can
+
+library(caTools)
+sample = sample.split(s_data$Scores, SplitRatio = 0.70)
+
+trainingdata = subset(s_data, sample == 'TRUE')
+testingdata = subset(s_data, sample =='FALSE')
+
+
+#now that data is divided, we will create a model
+library(MLmetrics)
+percentageModel = lm(Scores~. , data = trainingdata)
+summary(percentageModel)
+
+#predict this model using testset
+#before that remove dependent variable from test set
+testingdata_new = testingdata[-2]
+
+#now testing
+predictedScore = predict(percentageModel, testingdata_new)
+checkTable = cbind(Actual = testingdata$Scores, Predicted = predictedScore)
+
+#add this predictedScore value in Testingdata
+testingdata$PredScores = predictedScore
+
+#now check(validate) the difference through MAE(Mean absolute error)
+MAE(predictedScore, testingdata$Scores) 
+MAPE(predictedScore, testingdata$Scores) 
+
+#ANSWER 1 : predicted percentage based on hours = PredScore column in testingdata
+
+#ANSWER FOR Q2: WHAT IS PREDICTED SCORE IF STUDENT STUDIES 9.25 HOURS PER DAY
+library(dplyr)
+a = filter(testingdata, Hours >= 9.0 )$PredScores
+a
+
